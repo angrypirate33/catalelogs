b62fceee49510c38dfe510e0dc07bd2bb08c1e15
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-05-15 13:49:15 -0400

    Adds initial routers for beers and catalelogs.

---
+++ routes/beers.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+
+const beerCtrl = require('../controllers/beers')
+
+
+module.exports = router
---
+++ routes/catalelogs.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+
+const catalelogCtrl = require('../controllers/catalelogs')
+
+
+
+module.exports = router
