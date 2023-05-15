5ec23fc0bdbb576f9d270b4cb21cabbda93afab3
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-05-15 14:08:14 -0400

    Adds catalelog routing.

--- routes/catalelogs.js
+++ routes/catalelogs.js
@@ -3,6 +3,12 @@ const router = express.Router()
 
 const catalelogCtrl = require('../controllers/catalelogs')
 
-
+router.get('/',  catalelogCtrl.index)
+router.get('/new', catalelogCtrl.newCatalelog)
+router.post('/', catalelogCtrl.create)
+router.get('/:id', catalelogCtrl.show)
+router.get('/:id/edit', catalelogCtrl.updateCatalelogForm)
+router.put('/:id', catalelogCtrl.update)
+router.delete('/:id', catalelogCtrl.deleteCatalelog)
 
 module.exports = router
