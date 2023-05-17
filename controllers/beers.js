9f40ec15af5e62df8f386f4e3b04cafe132aaec3
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-05-17 18:51:14 -0400

    Adds API indes functionality.

--- controllers/beers.js
+++ controllers/beers.js
@@ -1,9 +1,32 @@
 const Catalelog = require('../models/catalelog')
 
 module.exports = {
+    index,
     show,
     addBeerToCatalelog,
     deleteBeerFromCatalelog
+}
+
+function index(req, res, next) {
+    const encodedKey = process.env.ENCODED_KEY
+    const options = {
+        headers: {
+            Authorization: `Basic ${encodedKey}`,
+            Accept: 'application/json'
+        }
+    }
+    fetch('https://api.catalog.beer/beer?count=10%cursor=', options)
+    .then(res => {
+        if (res.ok) return res.json();
+    })    
+    .then(beers => {
+        res.render('beers/index', {
+            beers: beers.data,
+            title: 'Beer List'
+        })
+        console.log(beers.data)
+    })
+    .catch(next)
 }
 
 function show(req, res, next) {
--- routes/beers.js
+++ routes/beers.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const beerCtrl = require('../controllers/beers')
 
+router.get('/', beerCtrl.index)
 router.get('/:name', beerCtrl.show)
 router.post('/:catalelogId', beerCtrl.addBeerToCatalelog)
 router.delete('/:catalelogId/:beerId', beerCtrl.deleteBeerFromCatalelog)
