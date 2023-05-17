81af92b10cbd1adc8bdb5e13442e9cc8f25aa297
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-05-17 11:43:42 -0400

    Updates add beer form to search the API. Creates searchBeers function in beers controller. Encodes API key in base64 as required by API.

--- controllers/beers.js
+++ controllers/beers.js
@@ -1,9 +1,11 @@
 const Catalelog = require('../models/catalelog')
+const encodedKey = base64_encode('c5084d96-3d59-48a0-b9d8-df5a31695d9e:')
 
 module.exports = {
     show,
     addBeerToCatalelog,
-    deleteBeerFromCatalelog
+    deleteBeerFromCatalelog,
+    searchBeers
 }
 
 function show(req, res, next) {
@@ -31,4 +33,31 @@ function deleteBeerFromCatalelog(req, res, next) {
         })
         .then(() => res.redirect(`/catalelogs/${req.params.catalelogId}`))
         .catch(next)
+}
+
+function searchBeers(req, res, next) {
+    const name = document.getElementById("name").value
+    const url = `https://api.catalog.beer/beers?name=${name}`
+    const request = new Request(url, {
+    method: 'GET',
+    headers: {
+      'Authorization': `Basic ${encodedKey}`,
+    },
+    })
+    fetch(request)
+        .then(response => {
+            if (response.status === 200) {
+            const beers = response.json()
+            const list = document.getElementById("beers-list")
+            list.innerHTML = ""
+
+        beers.forEach(beer => {
+            const li = document.createElement("li");
+            li.textContent = beer.name;
+            list.appendChild(li);
+        })
+        } else {
+            console.log(`Error ${response.status}`);
+        }   
+        })
 }
